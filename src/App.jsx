0c16a0fd0ea0a97c5a3d4099d6246f2d22333ccd
S Abhishek <abhishekgowtam102007@gmail.com>
2026-05-25 21:58:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import LandingPage from './components/landingpage.jsx';
 import HighlightsSection from './components/Highlights.jsx';
-import HackerDesktop from './components/Team.jsx';
+import Team from './components/Team.jsx';
 
 export default function App() {
   return (
